22e891e1c86541f823c2d6353a37d848c558c2e9
ChiragChrg <chiruchirag2001@gmail.com>
2023-10-28 13:35:19 +0530

    Dynamic Header witch customizable Logo ✨

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
-import { Logo } from '@/assets'
-import Image from 'next/image'
 import Link from 'next/link'
 import ThemeButton from './Buttons/ThemeButton'
+import Logo from '@/assets/Logo'
 
-const Header = () => {
+type Props = {
+    altLogo?: boolean
+}
+
+const Header = ({ altLogo = false }: Props) => {
     return (
         <header className='flex justify-between items-center z-10'>
-            <div className="flex_center gap-3 text-[var(--logoClr)]">
-                <Image src={Logo} alt='ARMS_Logo' width={40} height={40} />
-                <p className='text-[2em] font-bold leading-8'>ARMS</p>
+            <div className="flex_center gap-3">
+                <Logo
+                    size='35'
+                    fill={altLogo ? "#fff" : "var(--logoClr)"}
+                    stroke={altLogo ? "var(--logoClr)" : "rgba(0, 0, 0, 0.5)"} />
+                <p
+                    style={{
+                        color: altLogo ? "#fff" : "var(--logoClr)"
+                    }}
+                    className='text-[2em] font-bold leading-8'>ARMS</p>
             </div>
 
-            <nav className='flex items-center gap-8 text-[1.1em] text-white'>
+            <nav
+                style={{
+                    color: !altLogo ? "#fff" : ""
+                }}
+                className='flex items-center gap-8 text-[1.1em]'>
                 <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
                 <Link href="https://chiragchrg.netlify.app/" target='_blank'>Portfolio</Link>
                 <Link href="https://github.com/ChiragChrg" target='_blank'>GitHub</Link>
 
