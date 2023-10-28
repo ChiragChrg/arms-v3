ab3eac68d4179cb5db011967e54e11b68fdd9a73
ChiragChrg <chiruchirag2001@gmail.com>
2023-10-29 00:10:15 +0530

    Modified Trails Ui to align at center ✨🔥

--- src/components/Trails.tsx
+++ src/components/Trails.tsx
@@ -8,37 +8,37 @@ type Props = {
     trailCount?: number,
     xOffset?: string,
     rotate?: string,
-    topOffset?: string,
     leftBtn?: boolean
 }
 
-const Trails = ({ trailCount = 10, xOffset = "-70%", rotate = "-45deg", topOffset = "50%", leftBtn = false }: Props) => {
+const Trails = ({ trailCount = 10, xOffset = "45%", rotate = "-45deg", leftBtn = false }: Props) => {
     const [disableAnimation, setDisableAnimation] = useState<boolean>(false)
     const trailArray = Array.from({ length: trailCount }, (_, index) => index + 1);
 
     return (
         <>
-            <div
-                style={{
-                    rotate: rotate,
-                    transform: `translateY(${xOffset})`,
-                    top: topOffset
-                }}
-                className={`fixed w-[200%] h-[80%] bg-primary -z-10`}>
-                <div className="relative flex flex-col justify-evenly w-full h-full">
-                    {/* <div className="absolute flex_center w-full h-full">
-                        <div className=" w-[100px] h-full bg-red-600"></div>
-                    </div> */}
-                    {trailArray.map((item: number) => (
-                        <div
-                            key={item}
-                            data-disableanimation={disableAnimation}
-                            style={{
-                                backgroundColor: `${item % 2 == 0 ? "var(--trailClrA)" : "var(--trailClrB)"}`,
-                                height: `calc(100% / (${trailCount} * 3))`
-                            }}
-                            className='Trails-Trail w-full rounded-full' />
-                    ))}
+            <div className="fixed inset-0 flex_center w-screen h-screen overflow-hidden -z-10">
+                <div
+                    style={{
+                        rotate: rotate,
+                        transform: `translateY(${xOffset})`,
+                    }}
+                    className={`fixed w-[200%] h-[80%] bg-primary`}>
+                    <div className="relative flex flex-col justify-evenly w-full h-full">
+                        {/* <div className="absolute flex_center w-full h-full">
+                            <div className=" w-[100px] h-full bg-red-600"></div>
+                        </div> */}
+                        {trailArray.map((item: number) => (
+                            <div
+                                key={item}
+                                data-disableanimation={disableAnimation}
+                                style={{
+                                    backgroundColor: `${item % 2 == 0 ? "var(--trailClrA)" : "var(--trailClrB)"}`,
+                                    height: `calc(100% / (${trailCount} * 3))`
+                                }}
+                                className='Trails-Trail w-full rounded-full' />
+                        ))}
+                    </div>
                 </div>
             </div>
 
