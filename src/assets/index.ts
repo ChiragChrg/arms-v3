6a1522bde2624b5b0651411844ba5fb05213ca47
ChiragChrg <chiruchirag2001@gmail.com>
2023-10-29 13:21:40 +0530

    Added Login Page Vector arts ✨

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,3 @@
 export { default as LandingVector } from './LandingVector.svg'
+export { default as PeopleVector } from './PeopleVector.svg'
+export { default as UploadVector } from './UploadVector.svg'
