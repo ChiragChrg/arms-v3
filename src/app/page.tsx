594a06ed7cca397cbb09de458c02bd3609dec437
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-02 00:11:09 +0530

    Landing page Responsive UI Done ✨👍🏻

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,25 +18,24 @@ export default function Home() {
           <p className='text-[1em] 2xl:text-[1.5em]'>
             ARMS is an open-source software developed for students. <br />
             Students can easily find and download the study materials uploaded by the college faculty.</p>
-
         </div>
 
         <div className="flex justify-between items-end mt-[5em] 2xl:mt-[10em]">
-          <Image src={LandingVector} alt='LandingVector' className='w-[350px] 2xl:w-[500px]' />
+          <Image src={LandingVector} alt='LandingVector' className='hidden sm:block w-[350px] 2xl:w-[500px]' />
 
-          <div className="flex_center flex-col gap-8 bg-background/70 backdrop-blur-md rounded-md p-4 2xl:p-8 mr-[17em] 2xl:mr-[25em]">
+          <div className="w-full sm:w-fit flex_center flex-col gap-8 bg-background/70 backdrop-blur-md rounded-md p-4 2xl:p-8 sm:mr-[17em] 2xl:mr-[25em]">
             <h2 className='text-[1.4em]'>Let&apos;s get started! 🚀</h2>
-            <div className="flex_center gap-4">
+            <div className="flex_center gap-4 w-full sm:w-fit">
               <Link
                 href='/dashboard'
-                className='flex_center flex-col rounded w-[10em] py-2 px-2 2xl:px-4 bg-background hover:text-white hover:bg-[var(--logoClr)] transition-colors transi cursor-pointer'>
+                className='flex_center flex-col rounded w-full sm:w-[10em] py-2 px-2 2xl:px-4 bg-background hover:text-white hover:bg-[var(--logoClr)] transition-colors transi cursor-pointer'>
                 <h3 className='text-[1.1em]'>I&apos;m a Student</h3>
                 <p className='opacity-70 text-[0.9em]'>Anonymous</p>
               </Link>
 
               <Link
                 href="/login"
-                className='flex_center flex-col rounded w-[10em] py-2 px-2 2xl:px-4 bg-background hover:text-white hover:bg-[var(--logoClr)] transition-colors transi cursor-pointer'>
+                className='flex_center flex-col rounded w-full sm:w-[10em] py-2 px-2 2xl:px-4 bg-background hover:text-white hover:bg-[var(--logoClr)] transition-colors transi cursor-pointer'>
                 <h3 className='text-[1.1em]'>I&apos;m a Faculty</h3>
                 <p className='opacity-70 text-[0.9em]'>Login / Signup</p>
               </Link>
@@ -48,7 +47,7 @@ export default function Home() {
       </section>
 
 
-      <Trails className='2xl:h-[75%]' />
+      <Trails className='w-[400%] h-[40%] sm:w-[200%] sm:h-[85%] 2xl:h-[75%]' />
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import ThemeButton from './CustomUI/ThemeButton'
 import Logo from '@/assets/Logo'
 import { cn } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
+import { MenuIcon, X } from 'lucide-react'
 
 type Props = {
     altLogo?: boolean,
@@ -12,6 +13,7 @@ type Props = {
 }
 
 const Header = ({ altLogo = false, altColor = false }: Props) => {
+    const [showNav, setShowNav] = useState<boolean>(false)
     const router = useRouter()
 
     useEffect(() => {
@@ -21,6 +23,12 @@ const Header = ({ altLogo = false, altColor = false }: Props) => {
             router.push('/dashboard')
         }
     }, [router])
+
+    useEffect(() => {
+        if (showNav) {
+            document.body.style.userSelect = "none"
+        }
+    }, [showNav])
 
     return (
         <header className='flex justify-between items-center z-10'>
@@ -36,8 +44,29 @@ const Header = ({ altLogo = false, altColor = false }: Props) => {
                     className='text-[2em] font-bold leading-8'>ARMS</p>
             </div>
 
+            <MenuIcon size={40} className='sm:hidden text-logoClr dark:text-white' onClick={() => setShowNav(true)} />
+
+            <div
+                style={{
+                    transform: `translateX(${showNav ? "0" : "-100%"})`,
+                    pointerEvents: showNav ? "auto" : "none",
+                    userSelect: showNav ? "auto" : "none",
+                }}
+                className='sm:hidden fixed inset-0 flex flex-col bg-background/50 backdrop-blur-sm z-50'>
+                <nav className='w-[60%] h-full ml-auto flex flex-col justify-start items-center gap-4 pt-8 bg-background shadow text-[1.5em] tracking-wider'>
+                    <X size={50} className='bg-primary/80 text-white rounded-full p-1 mb-8' onClick={() => setShowNav(false)} />
+
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="https://chiragchrg.netlify.app/" target='_blank'>Portfolio</Link>
+                    <Link href="https://github.com/ChiragChrg" target='_blank'>GitHub</Link>
+
+                    <ThemeButton size={40} className='mt-8 dark:bg-background dark:text-white' />
+                </nav>
+            </div>
+
             <nav
-                className={cn('flex items-center gap-8 text-[1.1em] drop-shadow-md', altColor && "text-white")}>
+                className={cn('hidden sm:flex items-center gap-8 text-[1.1em] drop-shadow-md', altColor && "text-white")}>
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
                 <Link href="https://chiragchrg.netlify.app/" target='_blank'>Portfolio</Link>
