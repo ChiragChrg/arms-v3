797d1b301fc75aa52d780b27a42a418b8a61e13b
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-10 23:59:06 +0530

    Modified ThemeBtn to update theme-color Meta Tag ✨

--- src/components/CustomUI/ThemeButton.tsx
+++ src/components/CustomUI/ThemeButton.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useRef } from 'react'
 import { SunIcon, MoonIcon } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "../ui/button"
@@ -11,12 +12,25 @@ type Props = {
 
 const ThemeButton = ({ size = 24, className = "" }: Props) => {
     const { setTheme, resolvedTheme } = useTheme()
+    const themeMetaTagRef = useRef<HTMLElement | null>(null)
+
+    const HandleThemeToggle = () => {
+        themeMetaTagRef.current = document?.querySelector('meta[name="theme-color"]');
+        
+        if (resolvedTheme === "dark") {
+            setTheme("light")
+            themeMetaTagRef.current?.setAttribute('content', "hsl(0 0% 100%)");
+        } else {
+            setTheme("dark")
+            themeMetaTagRef.current?.setAttribute('content', "hsl(222.2 84% 4.9%)");
+        }
+    }
 
     return (
         <Button variant="ghost" size="icon"
             className={cn("rounded-full bg-transparent border border-white/30 sm:hover:bg-white sm:hover:text-baseClr", className)}
             title={resolvedTheme === "dark" ? "Toggle Light Mode" : "Toggle Dark Mode"}
-            onClick={() => resolvedTheme === "dark" ? setTheme("light") : setTheme("dark")}>
+            onClick={HandleThemeToggle}>
             <SunIcon size={size} className="dark:hidden" />
             <MoonIcon size={size} className="hidden dark:block" />
         </Button>
