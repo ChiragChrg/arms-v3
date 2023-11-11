e4095e5ece6c4e69880cbc88b3eb0fbcdfc8f2d7
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-11 23:01:07 +0530

    Minor Changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
         <main className='relative flex flex-col px-4 py-3 w-full h-full min-h-screen overflow-hidden'>
             <Header disableAuth={true} />
 
-            <section className='w-full h-full flex justify-between items-center flex-col px-8 2xl:px-4'>
+            <section className='w-full h-full flex justify-between items-center flex-col px-3 2xl:px-4'>
                 <div className="relative flex_center w-full max-w-[240px] h-[240px]">
                     <span className='absolute w-full aspect-square rounded-full bg-primary/80 opacity-0 animate-ripple'></span>
                     <span className='absolute w-full aspect-square rounded-full bg-primary/80 opacity-0 animate-ripple delay-700'></span>
