98e0d49c618db75187ca0512d9590d021cca7385
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-11 22:51:54 +0530

    Dynamic ROOT Path & disableAuth redirect for public pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,19 +13,26 @@ import useLoaderStore from '@/store/useLoaderStore'
 type Props = {
     altLogo?: boolean,
     altColor?: boolean,
-    className?: string
+    className?: string,
+    disableAuth?: boolean
 }
 
-const Header = ({ altLogo = false, altColor = false, className = "" }: Props) => {
+type HomeRouteType = {
+    path: string,
+    name: string
+}
+
+const Header = ({ altLogo = false, altColor = false, className = "", disableAuth = false }: Props) => {
     const [showNav, setShowNav] = useState<boolean>(false)
+    const [homeRoute, setHomeRoute] = useState<HomeRouteType>({ path: "/", name: "Home" })
     const router = useRouter()
-    const { setUser } = useUserStore()
+    const { user, setUser } = useUserStore()
     const { setShowLoader } = useLoaderStore()
 
     useEffect(() => {
         const localUser = JSON.parse(localStorage.getItem('arms-user') as string)
 
-        if (localUser?.uid) {
+        if (localUser?.uid && !disableAuth) {
             setUser(localUser)
             router.push('/dashboard')
 
@@ -44,6 +51,15 @@ const Header = ({ altLogo = false, altColor = false, className = "" }: Props) =>
             document.body.style.userSelect = "none"
         }
     }, [showNav])
+
+    useEffect(() => {
+        if (user?.uid) {
+            setHomeRoute({
+                path: "/dashboard",
+                name: "Dashboard"
+            })
+        }
+    }, [user])
 
     return (
         <header className={cn('flex justify-between items-center z-20', className)}>
@@ -73,7 +89,7 @@ const Header = ({ altLogo = false, altColor = false, className = "" }: Props) =>
                 <nav className='w-[60%] h-full ml-auto flex flex-col justify-start items-center gap-4 pt-8 bg-background shadow text-[1.5em] tracking-wider'>
                     <X size={45} className='bg-primary/80 text-white rounded-full p-2 mb-8' onClick={() => setShowNav(false)} />
 
-                    <Link href="/">Home</Link>
+                    <Link href={homeRoute?.path}>{homeRoute?.name}</Link>
                     <Link href="/about">About</Link>
                     <Link href="https://chiragchrg.netlify.app/" target='_blank'>Portfolio</Link>
                     <Link href="https://github.com/ChiragChrg" target='_blank'>GitHub</Link>
@@ -84,7 +100,7 @@ const Header = ({ altLogo = false, altColor = false, className = "" }: Props) =>
 
             <nav
                 className={cn('hidden sm:flex items-center gap-8 text-[1.1em] drop-shadow-md', altColor && "text-white")}>
-                <Link href="/">Home</Link>
+                <Link href={homeRoute?.path}>{homeRoute?.name}</Link>
                 <Link href="/about">About</Link>
                 <Link href="https://chiragchrg.netlify.app/" target='_blank'>Portfolio</Link>
                 <Link href="https://github.com/ChiragChrg" target='_blank'>GitHub</Link>
