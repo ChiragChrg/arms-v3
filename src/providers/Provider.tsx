3623ca4aec3544ccdfd3f4cf84746d8c15b62886
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-17 18:57:50 +0530

    Modified QueryClient Options ✨

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -15,6 +15,8 @@ const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            retry: 1,
         },
     },
 })
