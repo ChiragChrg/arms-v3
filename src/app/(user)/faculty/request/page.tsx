aa09e76554e62d8a6441ee7b21178278af1bc407
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-25 16:28:56 +0530

    Added separate api route for pending faculty ✨

--- src/app/(user)/faculty/request/page.tsx
+++ src/app/(user)/faculty/request/page.tsx
@@ -6,6 +6,7 @@ import { useQuery } from '@tanstack/react-query'
 import { Button } from '@/components/ui/button'
 import NavRoute from '@/components/NavRoutes'
 import { CheckIcon, Loader, X } from 'lucide-react'
+import { EmptyApprovalVector } from '@/assets'
 
 interface FacultyType {
     uid: string,
@@ -18,9 +19,9 @@ interface FacultyType {
 
 const Request = () => {
     const { data: users } = useQuery({
-        queryKey: ["facultyList", "facultyRequestList"],
+        queryKey: ["facultyRequestList"],
         queryFn: async () => {
-            const { data } = await axios.get("/api/getfaculty")
+            const { data } = await axios.get("/api/getapproval")
             return data as FacultyType[]
         }
     })
@@ -35,40 +36,47 @@ const Request = () => {
                 <span className="text-primary"> Request</span>
             </h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
-                {users?.map((user, index) => {
-                    if (!user?.isApproved) return //ifTruw return
+            {users?.length !== 0 ?
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
+                    {users?.map((user, index) => {
+                        if (!user?.isApproved) return //ifTruw return
 
-                    return (
-                        <div key={index} className="flex_center flex-col gap-1 rounded-md py-4 bg-radialGradientDark dark:bg-radialGradientDark">
-                            <Image
-                                src={user?.avatarImg}
-                                width={80}
-                                height={80}
-                                alt='User_Image'
-                                className='rounded-full' />
+                        return (
+                            <div key={index} className="flex_center flex-col gap-1 rounded-md py-4 bg-radialGradientDark dark:bg-radialGradientDark">
+                                <Image
+                                    src={user?.avatarImg}
+                                    width={80}
+                                    height={80}
+                                    alt='User_Image'
+                                    className='rounded-full' />
 
-                            <h3>{user?.username}</h3>
-                            <span className='text-[0.8em] opacity-80'>{user?.email}</span>
+                                <h3>{user?.username}</h3>
+                                <span className='text-[0.8em] opacity-80'>{user?.email}</span>
 
-                            <div className="flex_center gap-4 mt-2 w-full px-8">
-                                <Button className='flex_center gap-1 px-2 py-1 min-w-[90px] w-full h-fit text-[0.8em] bg-green-500 hover:bg-green-600 text-white drop-shadow'>
-                                    {false ?
-                                        <Loader size={20} className='animate-spin' />
-                                        :
-                                        <CheckIcon size={20} />}
-                                    <span>Approve</span>
-                                </Button>
+                                <div className="flex_center gap-4 mt-2 w-full px-8">
+                                    <Button className='flex_center gap-1 px-2 py-1 min-w-[90px] w-full h-fit text-[0.8em] bg-green-500 hover:bg-green-600 text-white drop-shadow'>
+                                        {false ?
+                                            <Loader size={20} className='animate-spin' />
+                                            :
+                                            <CheckIcon size={20} />}
+                                        <span>Approve</span>
+                                    </Button>
 
-                                <Button variant="destructive" className='flex_center gap-1 px-2 py-1 min-w-[90px] w-full h-fit text-[0.8em] text-white drop-shadow'>
-                                    <X size={20} />
-                                    <span>Reject</span>
-                                </Button>
+                                    <Button variant="destructive" className='flex_center gap-1 px-2 py-1 min-w-[90px] w-full h-fit text-[0.8em] text-white drop-shadow'>
+                                        <X size={20} />
+                                        <span>Reject</span>
+                                    </Button>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </div>
+                        )
+                    })}
+                </div>
+                :
+                <div className="flex_center flex-col gap-4 h-[80%]">
+                    <Image src={EmptyApprovalVector} width={250} height={250} alt='EmptyApprovalVector' className='md:w-[400px]' />
+                    <span className='text-[1.25em]'>No new faculty requests</span>
+                </div>
+            }
         </section>
     )
 }
--- src/app/api/getapproval/route.ts
+++ src/app/api/getapproval/route.ts
@@ -5,9 +5,9 @@ import UserModel from "@/models/UserModel";
 export async function GET(request: Request) {
     try {
         await connectDB();
-        const faculty = await UserModel.find({ isApproved: true })
+        const users = await UserModel.find({ isApproved: false })
 
-        return new NextResponse(JSON.stringify(faculty), { status: 200 });
+        return new NextResponse(JSON.stringify(users), { status: 200 });
     } catch (err) {
         console.error(err);
         return new NextResponse(JSON.stringify(err), { status: 500 });
