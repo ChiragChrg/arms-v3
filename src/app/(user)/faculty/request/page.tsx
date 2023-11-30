cd14f9f6bb9ddfb790a4f6db7f335feec021e5cb
ChiragChrg <chiruchirag2001@gmail.com>
2023-11-30 20:38:49 +0530

    Moved User request cards to separate component👍🏻

--- src/app/(user)/faculty/request/page.tsx
+++ src/app/(user)/faculty/request/page.tsx
@@ -3,13 +3,12 @@ import Image from 'next/image'
 import axios from 'axios'
 import MobileHeader from '@/components/MobileHeader'
 import { useQuery } from '@tanstack/react-query'
-import { Button } from '@/components/ui/button'
 import NavRoute from '@/components/NavRoutes'
-import { CheckIcon, Loader, X } from 'lucide-react'
 import { EmptyApprovalVector } from '@/assets'
+import UserCard from './UserCard'
 
-interface FacultyType {
-    uid: string,
+export interface FacultyType {
+    _id: string,
     username: string,
     email: string,
     avatarImg: string,
@@ -39,35 +38,10 @@ const Request = () => {
             {users?.length !== 0 ?
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
                     {users?.map((user, index) => {
-                        if (!user?.isApproved) return //ifTruw return
+                        if (user?.isApproved) return //if True return
 
                         return (
-                            <div key={index} className="flex_center flex-col gap-1 rounded-md py-4 bg-radialGradientDark dark:bg-radialGradientDark">
-                                <Image
-                                    src={user?.avatarImg}
-                                    width={80}
-                                    height={80}
-                                    alt='User_Image'
-                                    className='rounded-full' />
-
-                                <h3>{user?.username}</h3>
-                                <span className='text-[0.8em] opacity-80'>{user?.email}</span>
-
-                                <div className="flex_center gap-4 mt-2 w-full px-8">
-                                    <Button className='flex_center gap-1 px-2 py-1 min-w-[90px] w-full h-fit text-[0.8em] bg-green-500 hover:bg-green-600 text-white drop-shadow'>
-                                        {false ?
-                                            <Loader size={20} className='animate-spin' />
-                                            :
-                                            <CheckIcon size={20} />}
-                                        <span>Approve</span>
-                                    </Button>
-
-                                    <Button variant="destructive" className='flex_center gap-1 px-2 py-1 min-w-[90px] w-full h-fit text-[0.8em] text-white drop-shadow'>
-                                        <X size={20} />
-                                        <span>Reject</span>
-                                    </Button>
-                                </div>
-                            </div>
+                            <UserCard key={index} user={user} />
                         )
                     })}
                 </div>
