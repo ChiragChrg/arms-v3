e127ab9a41b87130d202e9b915b5882d473173fe
ChiragChrg <chiruchirag2001@gmail.com>
2023-12-19 19:04:36 +0530

    Conditionally rendering recentTopics

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -96,22 +96,26 @@ const Dashboard = () => {
                 </div>
             }
 
-            <h2 className='text-[1.3em] sm:text-[1.6em] font-medium mt-6 mb-2'>
-                Recent
-                <span className="text-primary"> Topics</span>
-            </h2>
+            {recentTopic &&
+                <>
+                    <h2 className='text-[1.3em] sm:text-[1.6em] font-medium mt-6 mb-2'>
+                        Recent
+                        <span className="text-primary"> Topics</span>
+                    </h2>
 
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-                {recentTopic?.map((data, index) => (
-                    <Link href={data?.url} key={index} className="bg-primary/60 p-2 px-4 flex justify-start gap-6 rounded-md text-white">
-                        <OpenBookSVG size='50' />
-                        <div className="flex flex-col">
-                            <span className="text-[0.8em] opacity-70 capitalize">{data?.subtitle}</span>
-                            <h3 className="text-[1.2em] font-bold uppercase">{data?.title}</h3>
-                        </div>
-                    </Link>
-                ))}
-            </div>
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
+                        {recentTopic?.map((data, index) => (
+                            <Link href={data?.url} key={index} className="bg-primary/60 p-2 px-4 flex justify-start gap-6 rounded-md text-white">
+                                <OpenBookSVG size='50' />
+                                <div className="flex flex-col">
+                                    <span className="text-[0.8em] opacity-70 capitalize">{data?.subtitle}</span>
+                                    <h3 className="text-[1.2em] font-bold uppercase">{data?.title}</h3>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </>
+            }
         </section>
     )
 }
