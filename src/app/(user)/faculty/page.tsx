d5de8431286ea82f208dcfb388cfbfcbe779b14d
ChiragChrg <chiruchirag2001@gmail.com>
2024-01-12 19:30:16 +0530

    Render faculty pages only if User is Admin

--- src/app/(user)/faculty/page.tsx
+++ src/app/(user)/faculty/page.tsx
@@ -49,97 +49,98 @@ const Faculty = () => {
         refetchOnMount: true
     })
 
-    return (
-        <section className='section_style'>
-            <MobileHeader />
+    if (isAdmin)
+        return (
+            <section className='section_style'>
+                <MobileHeader />
 
-            <div className="flex justify-between items-center">
-                <h1 className='text-[1.6em] sm:text-[2em] font-bold'>
-                    <span className='hidden sm:inline'>Approved</span>
-                    <span className="sm:text-primary"> Faculty</span>
-                </h1>
+                <div className="flex justify-between items-center">
+                    <h1 className='text-[1.6em] sm:text-[2em] font-bold'>
+                        <span className='hidden sm:inline'>Approved</span>
+                        <span className="sm:text-primary"> Faculty</span>
+                    </h1>
 
-                {isAdmin &&
-                    <Link href="./faculty/request" className='flex_center gap-2 text-[1em] bg-primary text-white rounded-sm px-2 py-1.5'>
-                        <UserCheck />
-                        <span className='hidden sm:block'>Pending</span>
-                        <span>Request ( {data?.filter(user => user?.isApproved === false).length || 0} )</span>
-                    </Link>
-                }
-            </div>
+                    {isAdmin &&
+                        <Link href="./faculty/request" className='flex_center gap-2 text-[1em] bg-primary text-white rounded-sm px-2 py-1.5'>
+                            <UserCheck />
+                            <span className='hidden sm:block'>Pending</span>
+                            <span>Request ( {data?.filter(user => user?.isApproved === false).length || 0} )</span>
+                        </Link>
+                    }
+                </div>
 
-            {/* Table Ui for Desktop screen */}
-            <Table className='hidden xl:table mt-4'>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="px-2 sm:px-4 py-2">Username</TableHead>
-                        <TableHead className="px-2 sm:px-4 py-2">Email</TableHead>
-                        <TableHead className='px-2 sm:px-4 py-2 sm:table-cell'>DOJ</TableHead>
-                        <TableHead className="px-2 sm:px-4 py-2 w-fit text-center">Action</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {data?.map((faculty, index) => {
-                        if (!faculty?.isApproved) return
+                {/* Table Ui for Desktop screen */}
+                <Table className='hidden xl:table mt-4'>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="px-2 sm:px-4 py-2">Username</TableHead>
+                            <TableHead className="px-2 sm:px-4 py-2">Email</TableHead>
+                            <TableHead className='px-2 sm:px-4 py-2 sm:table-cell'>DOJ</TableHead>
+                            <TableHead className="px-2 sm:px-4 py-2 w-fit text-center">Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {data?.map((faculty, index) => {
+                            if (!faculty?.isApproved) return
 
-                        // Date Formating
-                        const date = new Date(faculty?.createdAt);
-                        const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' };
-                        const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+                            // Date Formating
+                            const date = new Date(faculty?.createdAt);
+                            const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' };
+                            const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
 
-                        return (
-                            <TableRow key={index}>
-                                <TableCell className="px-2 sm:px-4 py-2 flex items-center gap-3">
-                                    <AvatarImage url={faculty?.avatarImg} size={40} />
+                            return (
+                                <TableRow key={index}>
+                                    <TableCell className="px-2 sm:px-4 py-2 flex items-center gap-3">
+                                        <AvatarImage url={faculty?.avatarImg} size={40} />
+                                        <span className='font-medium capitalize'>{faculty?.username}</span>
+                                    </TableCell>
+                                    <TableCell className="px-2 sm:px-4 py-2">{faculty?.email}</TableCell>
+                                    <TableCell className='px-2 sm:px-4 py-2 sm:table-cell'>{formattedDate}</TableCell>
+                                    <TableCell className='px-2 sm:px-4 py-2 sm:table-cell text-center'>
+                                        <Button size="icon" variant='ghost'>
+                                            <MoreVerticalIcon />
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+
+                {/* Card Ui for Mobile screen */}
+                <div className="grid xl:hidden grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+                    {data?.map((faculty, index) => (
+                        <div key={index} className="flex justify-between items-center border border-secondary rounded-md px-1 py-0.5">
+                            <div className="flex items-center gap-4">
+                                {faculty?.avatarImg ?
+                                    <Image
+                                        src={faculty?.avatarImg}
+                                        alt='User_Avatar'
+                                        width={40}
+                                        height={40}
+                                        loading='eager'
+                                        className='rounded-full'
+                                    />
+                                    :
+                                    <div className="bg-slate-500 w-fit p-1.5 rounded-full">
+                                        <User2 size={30} />
+                                    </div>
+                                }
+
+                                <div className="flex flex-col">
                                     <span className='font-medium capitalize'>{faculty?.username}</span>
-                                </TableCell>
-                                <TableCell className="px-2 sm:px-4 py-2">{faculty?.email}</TableCell>
-                                <TableCell className='px-2 sm:px-4 py-2 sm:table-cell'>{formattedDate}</TableCell>
-                                <TableCell className='px-2 sm:px-4 py-2 sm:table-cell text-center'>
-                                    <Button size="icon" variant='ghost'>
-                                        <MoreVerticalIcon />
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
-
-            {/* Card Ui for Mobile screen */}
-            <div className="grid xl:hidden grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-                {data?.map((faculty, index) => (
-                    <div key={index} className="flex justify-between items-center border border-secondary rounded-md px-1 py-0.5">
-                        <div className="flex items-center gap-4">
-                            {faculty?.avatarImg ?
-                                <Image
-                                    src={faculty?.avatarImg}
-                                    alt='User_Avatar'
-                                    width={40}
-                                    height={40}
-                                    loading='eager'
-                                    className='rounded-full'
-                                />
-                                :
-                                <div className="bg-slate-500 w-fit p-1.5 rounded-full">
-                                    <User2 size={30} />
+                                    <span className='text-[0.8em] opacity-80'>{faculty?.email}</span>
                                 </div>
-                            }
-
-                            <div className="flex flex-col">
-                                <span className='font-medium capitalize'>{faculty?.username}</span>
-                                <span className='text-[0.8em] opacity-80'>{faculty?.email}</span>
                             </div>
-                        </div>
 
-                        <Button size="icon" variant='ghost'>
-                            <MoreVerticalIcon />
-                        </Button>
-                    </div>
-                ))}
-            </div>
-        </section>
-    )
+                            <Button size="icon" variant='ghost'>
+                                <MoreVerticalIcon />
+                            </Button>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        )
 }
 
 export default Faculty
--- src/app/(user)/faculty/request/page.tsx
+++ src/app/(user)/faculty/request/page.tsx
@@ -39,34 +39,35 @@ const Request = () => {
         }
     })
 
-    return (
-        <section className='section_style'>
-            <MobileHeader />
-            <NavRoute routes={["faculty", "./faculty/request"]} />
+    if (isAdmin)
+        return (
+            <section className='section_style'>
+                <MobileHeader />
+                <NavRoute routes={["faculty", "./faculty/request"]} />
 
-            <h1 className='text-[1.6em] sm:text-[2em] font-bold'>
-                <span>Pending</span>
-                <span className="text-primary"> Request</span>
-            </h1>
+                <h1 className='text-[1.6em] sm:text-[2em] font-bold'>
+                    <span>Pending</span>
+                    <span className="text-primary"> Request</span>
+                </h1>
 
-            {users?.length !== 0 ?
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
-                    {users?.map((user, index) => {
-                        if (user?.isApproved) return //if True return
+                {users?.length !== 0 ?
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 my-4">
+                        {users?.map((user, index) => {
+                            if (user?.isApproved) return //if True return
 
-                        return (
-                            <UserCard key={index} user={user} />
-                        )
-                    })}
-                </div>
-                :
-                <div className="flex_center flex-col gap-4 h-[80%]">
-                    <Image src={EmptyApprovalVector} width={250} height={250} alt='EmptyApprovalVector' className='md:w-[400px]' />
-                    <span className='text-[1.25em]'>No new faculty requests</span>
-                </div>
-            }
-        </section>
-    )
+                            return (
+                                <UserCard key={index} user={user} />
+                            )
+                        })}
+                    </div>
+                    :
+                    <div className="flex_center flex-col gap-4 h-[80%]">
+                        <Image src={EmptyApprovalVector} width={250} height={250} alt='EmptyApprovalVector' className='md:w-[400px]' />
+                        <span className='text-[1.25em]'>No new faculty requests</span>
+                    </div>
+                }
+            </section>
+        )
 }
 
 export default Request
